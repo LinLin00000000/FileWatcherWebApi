ab58931d944a934d2f2ece3d916b6194bfd0dc57
Lin <i@linlin.me>
2024-01-06 12:19:46 +0800

    ✨ feat: 支持按键自动触发（需要管理员权限）
    并将代码格式化了一下

--- KeyPressScheduler.cs
+++ KeyPressScheduler.cs
@@ -1,99 +1,85 @@
-using TinyHotKey;
 using System.Runtime.InteropServices;
 using System.Text;
+using WindowsInput.Events;
 
-public class KeyPressScheduler
-{
+public class KeyPressScheduler {
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
 
     [DllImport("user32.dll")]
     public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
 
-    [DllImport("user32.dll")]
-    public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
-    private const int KEYEVENTF_KEYUP = 0x2;
-
     private readonly Random random = new();
-    private readonly TinyHotKeyInstance tinyHotKey = new();
-    private bool IsEnabled { get; set; } = true;
-    private readonly Key triggerKey;
+    private bool IsEnabled { get; set; } = false;
+    private readonly KeyCode triggerKey;
     private readonly string targetWindowTitle;
     private readonly int interval;
     private readonly int jitter;
 
-    // Key: See https://learn.microsoft.com/zh-cn/dotnet/api/system.windows.forms.keys
-    public KeyPressScheduler(Key toggleKey, Key triggerKey, string targetWindowTitle, int interval, int jitter)
-    {
+    // KeyCode: See https://learn.microsoft.com/zh-cn/dotnet/api/system.windows.forms.keys
+    public KeyPressScheduler(KeyCode toggleKey, KeyCode triggerKey, string targetWindowTitle, int interval, int jitter, ILogger<FileWatcherWebApi.Program>? logger) {
         this.triggerKey = triggerKey;
         this.targetWindowTitle = targetWindowTitle;
         this.interval = interval;
         this.jitter = jitter;
 
         // 注册快捷键的事件处理程序
-        tinyHotKey.RegisterHotKey(Modifier.Alt, toggleKey, () =>
-        {
-            if (IsTargetWindowActive())
-            {
-                IsEnabled = !IsEnabled;
-                Console.WriteLine("KeyPressScheduler " + (IsEnabled ? "enabled" : "disabled"));
-            }
+        var Keyboard = WindowsInput.Capture.Global.KeyboardAsync();
+        Keyboard.KeyEvent += (sender, e) => {
+            if (e.Data?.KeyDown?.Key == toggleKey) {
+                if (IsTargetWindowActive()) {
+                    IsEnabled = !IsEnabled;
+                    if (logger != null) {
+                        logger.LogInformation("KeyPressScheduler {Status}", IsEnabled ? "enabled" : "disabled");
+                    } else {
+                        Console.WriteLine("KeyPressScheduler " + (IsEnabled ? "enabled" : "disabled"));
+                    }
 
-            return Task.CompletedTask;
-        });
+                }
+            }
+        };
     }
 
-    private bool IsTargetWindowActive()
-    {
+    private bool IsTargetWindowActive() {
         IntPtr foregroundWindow = GetForegroundWindow();
         StringBuilder windowTitle = new(256);
         GetWindowText(foregroundWindow, windowTitle, windowTitle.Capacity);
-        Console.WriteLine(windowTitle);
+        // Console.WriteLine(windowTitle);
 
         return windowTitle.ToString() == targetWindowTitle;
     }
 
-    public void Start()
-    {
-        Thread keyPressThread = new(new ThreadStart(RunScheduler))
-        {
-            IsBackground = false
-        };
-        keyPressThread.Start();
-
+    public void Start() {
+        Task.Run(RunSchedulerAsync);
     }
 
-    private void RunScheduler()
-    {
-        while (true)
-        {
-            if (IsEnabled && IsTargetWindowActive())
-            {
-                Console.WriteLine("Should Press Key: " + triggerKey);
-                SimulateKeyPress(triggerKey);
+    private async Task RunSchedulerAsync() {
+        while (true) {
+            if (IsEnabled && IsTargetWindowActive()) {
+                // Console.WriteLine("Should Press Key: " + triggerKey);
+                await SimulateKeyPressAsync(triggerKey);
             }
 
             Thread.Sleep(interval + random.Next(-jitter, jitter));
         }
     }
 
-    private void SimulateKeyPress(Key key)
-    {
-        keybd_event((byte)key, 0, 0, 0);
-        keybd_event((byte)key, 0, KEYEVENTF_KEYUP, 0);
+    private static async Task SimulateKeyPressAsync(KeyCode key) {
+        await WindowsInput.Simulate.Events()
+            .Click(key)
+            .Invoke();
     }
 
-    public static void Test()
-    {
+    public static void Test() {
         Console.WriteLine("Test!");
-        
-        Key toggleKey = Key.O; // 指定用于启用/停用的快捷键
-        Key triggerKey = Key.A; // 指定触发键
-        string targetWindowTitle = "微信";
+
+        KeyCode toggleKey = KeyCode.O; // 指定用于启用/停用的快捷键
+        KeyCode triggerKey = KeyCode.A; // 指定触发键
+        string targetWindowTitle = "微信"; // 微信 or EscapeFromTarkov
         int interval = 2000; // 指定的时间间隔（毫秒）
         int jitter = 500; // 抖动时间
 
-        KeyPressScheduler scheduler = new(toggleKey, triggerKey, targetWindowTitle, interval, jitter);
+        KeyPressScheduler scheduler = new(toggleKey, triggerKey, targetWindowTitle, interval, jitter, null);
         scheduler.Start();
         Console.WriteLine("Start!");
     }
--- Program.cs
+++ Program.cs
@@ -1,142 +1,109 @@
 using System.Net;
-using TinyHotKey;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using WindowsInput.Events;
 
-namespace FileWatcherWebApi
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace FileWatcherWebApi {
+    public class Program {
+        public static void Main(string[] args) {
+            // KeyPressScheduler.Test();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
+                .ConfigureAppConfiguration((hostingContext, config) => {
                     config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
                 })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+                .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
-                    // webBuilder.UseUrls("http://localhost:7544");
-                    
-                    webBuilder.ConfigureKestrel((context, options) =>
-                    {
-                        var loggerFactory = LoggerFactory.Create(builder =>
-                        {
+                    webBuilder.ConfigureKestrel((context, options) => {
+                        var loggerFactory = LoggerFactory.Create(builder => {
                             builder.AddConsole();
                         });
 
                         var logger = loggerFactory.CreateLogger<Program>();
-
                         var settings = context.Configuration.GetSection("ApplicationSettings");
-                        // bool hasError = false;
 
-                        // if (!Enum.TryParse<Key>(settings["ToggleKey"], true, out var toggleKey))
-                        // {
-                        //     logger.LogError("Invalid configuration for TriggerKey");
-                        //     hasError = true;
-                        // }
-
-                        // if (!Enum.TryParse<Key>(settings["TriggerKey"], true, out var triggerKey))
-                        // {
-                        //     logger.LogError("Invalid configuration for TriggerKey");
-                        //     hasError = true;
-                        // }
-
-                        // var targetWindowTitle = settings["TargetWindowTitle"];
-                        // if (string.IsNullOrEmpty(targetWindowTitle))
-                        // {
-                        //     logger.LogError("TargetWindowTitle configuration is missing");
-                        //     hasError = true;
-                        // }
-
-                        // if (!int.TryParse(settings["Interval_MS"], out var interval))
-                        // {
-                        //     logger.LogError("Invalid configuration for Interval_MS");
-                        //     hasError = true;
-                        // }
-
-                        // if (!int.TryParse(settings["Jitter_MS"], out var jitter))
-                        // {
-                        //     logger.LogError("Invalid configuration for Jitter_MS");
-                        //     hasError = true;
-                        // }
-
-                        // if (!IPAddress.TryParse(settings["Host"], out var host))
-                        // {
-                        //     logger.LogError("Invalid configuration for Host");
-                        //     hasError = true;
-                        // }
-
-                        // if (!int.TryParse(settings["Port"], out var port))
-                        // {
-                        //     logger.LogError("Invalid configuration for Port");
-                        //     hasError = true;
-                        // }
-
-                        // if (hasError)
-                        // {
-                        //     logger.LogError("Please check your appsettings.json file and ensure the settings are correct.");
-                        // }
-                        // else if (host != null)
-                        // {
-                        //     options.Listen(host, port);
-                        //     // KeyPressScheduler scheduler = new(toggleKey, triggerKey, targetWindowTitle, interval, jitter);
-                        //     // scheduler.Start();
-                        //     // KeyPressScheduler.Test();
-                        // }
-
-                        if (!string.IsNullOrEmpty(settings["Host"])
-                            && IPAddress.TryParse(settings["Host"], out var host)
-                            && int.TryParse(settings["Port"], out var port))
-                        {
-                            options.Listen(host, port);
-                        }
-                        else
-                        {
-                            options.Listen(IPAddress.Parse("127.0.0.1"), 7543);
-                        }
+                        UseKeyPressScheduler(logger, settings);
+                        SetHostAndPort(options, settings);
                     });
                 });
+
+        private static void SetHostAndPort(KestrelServerOptions options, IConfigurationSection settings) {
+            if (!string.IsNullOrEmpty(settings["Host"])
+                                        && IPAddress.TryParse(settings["Host"], out var host)
+                                        && int.TryParse(settings["Port"], out var port)) {
+                options.Listen(host, port);
+            } else {
+                options.Listen(IPAddress.Parse("127.0.0.1"), 7543);
+            }
+        }
+
+        private static void UseKeyPressScheduler(ILogger<Program> logger, IConfigurationSection settings) {
+            bool hasError = false;
+            if (!Enum.TryParse<KeyCode>(settings["ToggleKey"], true, out var toggleKey)) {
+                logger.LogInformation("Invalid configuration for TriggerKey");
+                hasError = true;
+            }
+
+            if (!Enum.TryParse<KeyCode>(settings["TriggerKey"], true, out var triggerKey)) {
+                logger.LogInformation("Invalid configuration for TriggerKey");
+                hasError = true;
+            }
+
+            var targetWindowTitle = settings["TargetWindowTitle"];
+            if (string.IsNullOrEmpty(targetWindowTitle)) {
+                logger.LogInformation("TargetWindowTitle configuration is missing");
+                hasError = true;
+            }
+
+            if (!int.TryParse(settings["Interval_MS"], out var interval)) {
+                logger.LogInformation("Invalid configuration for Interval_MS");
+                hasError = true;
+            }
+
+            if (!int.TryParse(settings["Jitter_MS"], out var jitter)) {
+                logger.LogInformation("Invalid configuration for Jitter_MS");
+                hasError = true;
+            }
+
+            if (hasError) {
+                logger.LogError("KeyPressScheduler 未启用");
+            } else if (targetWindowTitle != null) {
+                KeyPressScheduler scheduler = new(toggleKey, triggerKey, targetWindowTitle, interval, jitter, logger);
+                scheduler.Start();
+            }
+        }
     }
 
-    public class FileWatcherService
-    {
+    public class FileWatcherService {
         private readonly ILogger<FileWatcherService> _logger;
         private readonly FileSystemWatcher _watcher = new();
         private readonly List<StreamWriter> _clients = [];
         private readonly object _clientsLock = new();
 
-        public FileWatcherService(ILogger<FileWatcherService> logger)
-        {
+        public FileWatcherService(ILogger<FileWatcherService> logger) {
             _logger = logger;
             ConfigureWatcher();
         }
 
-        private void ConfigureWatcher()
-        {
+        private void ConfigureWatcher() {
             var watchFolder = Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
                 "Escape from Tarkov",
                 "Screenshots"
             );
 
-            if (!Directory.Exists(watchFolder))
-            {
+            if (!Directory.Exists(watchFolder)) {
                 Directory.CreateDirectory(watchFolder);
             }
 
             _watcher.Path = watchFolder;
             _watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
 
-            _watcher.Created += async (sender, e) =>
-            {
-                try
-                {
-                    if (!string.IsNullOrEmpty(e.Name))
-                    {
+            _watcher.Created += async (sender, e) => {
+                try {
+                    if (!string.IsNullOrEmpty(e.Name)) {
                         _logger.LogInformation($"New file detected: {e.FullPath}");
 
                         await SendFileNameToClientsAsync(e.Name);
@@ -144,9 +111,7 @@ namespace FileWatcherWebApi
                         await Task.Delay(TimeSpan.FromSeconds(3));
                         File.Delete(e.FullPath);
                     }
-                }
-                catch (Exception ex)
-                {
+                } catch (Exception ex) {
                     _logger.LogError(ex, "Exception in file watcher task");
                 }
             };
@@ -154,61 +119,46 @@ namespace FileWatcherWebApi
             _watcher.EnableRaisingEvents = true;
         }
 
-        private async Task SendFileNameToClientsAsync(string fileName)
-        {
+        private async Task SendFileNameToClientsAsync(string fileName) {
             List<StreamWriter> clientsSnapshot;
-            lock (_clientsLock)
-            {
+            lock (_clientsLock) {
                 clientsSnapshot = new List<StreamWriter>(_clients);
             }
 
-            foreach (var client in clientsSnapshot)
-            {
-                try
-                {
+            foreach (var client in clientsSnapshot) {
+                try {
                     await client.WriteAsync($"data: {fileName}\n\n");
                     await client.FlushAsync();
-                }
-                catch
-                {
-                    lock (_clientsLock)
-                    {
+                } catch {
+                    lock (_clientsLock) {
                         _clients.Remove(client);
                     }
                 }
             }
         }
 
-        public void AddClient(StreamWriter client)
-        {
-            lock (_clientsLock)
-            {
+        public void AddClient(StreamWriter client) {
+            lock (_clientsLock) {
                 _clients.Add(client);
             }
         }
 
-        public void RemoveClient(StreamWriter client)
-        {
-            lock (_clientsLock)
-            {
+        public void RemoveClient(StreamWriter client) {
+            lock (_clientsLock) {
                 _clients.Remove(client);
             }
         }
     }
 
-    public class Startup
-    {
+    public class Startup {
         public IConfiguration Configuration { get; }
 
-        public Startup(IConfiguration configuration)
-        {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddCors(options => {
                 options.AddPolicy("AllowAllOrigins",
                     builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             });
@@ -216,30 +166,23 @@ namespace FileWatcherWebApi
             services.AddSingleton<FileWatcherService>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, FileWatcherService fileWatcherService)
-        {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, FileWatcherService fileWatcherService) {
             app.UseCors("AllowAllOrigins");
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/map", async context =>
-                {
+            app.UseEndpoints(endpoints => {
+                endpoints.MapGet("/map", async context => {
                     context.Response.Headers.ContentType = "text/event-stream";
                     var responseWriter = new StreamWriter(context.Response.Body);
 
                     fileWatcherService.AddClient(responseWriter);
 
-                    try
-                    {
+                    try {
                         await Task.Delay(Timeout.Infinite, context.RequestAborted);
-                    }
-                    catch (TaskCanceledException)
-                    {
+                    } catch (TaskCanceledException) {
                         // 客户端断开连接
                     }
-                    finally
-                    {
+                    finally {
                         fileWatcherService.RemoveClient(responseWriter);
                     }
                 });
